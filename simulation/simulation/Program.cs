0aa9c0511d263a730142f0d712c1201c82ae844e
momo <herr_moise@msn.com>
2021-10-23 21:43:38 +0100

    define simulation

---
+++ simulation/simulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace simulation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter file name");
+
+            var FileName = Console.ReadLine();
+
+            Console.WriteLine("FileName: " + FileName);
+        }
+    }
+}
